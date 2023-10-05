f4d9e3bc6a25f35d340d7edc0ff20d4be6ac6bb3
venkatesh-548 <venkateshtaddi548@gmail.com>
2023-10-05 22:45:32 +0530

    up

---
+++ 03_js_operators/01_js_/app.js
@@ -0,0 +1,26 @@
+console.log("welcome to javascript")
+
+
+console.log("iam a log message");
+console.info("Iam an Info message");
+console.warn("iam a warning message");
+console.error("Iam a error message")
+
+let student={
+    name:"venky",
+    age:22,
+    branch:"cse"
+
+};
+console.table(student)
+
+
+let a=100;
+let b=200;
+let sum=a+b;
+console.log("the sum of "+a+" , " +b+" is: " +sum)//ES5 
+console.log(`the sum of ${a},${b} is :${sum}`) //ES6 version6
+
+let x
+
+console.log(x)
